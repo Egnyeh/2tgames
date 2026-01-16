8ae2b536805f0aec75fbfccf42e08d0fb27c178f
Egnyeh <egnyeh@gmail.com>
2026-01-16 19:51:38 +0100

    Fixes.

--- template/app/routers/orders.py
+++ template/app/routers/orders.py
@@ -7,6 +7,13 @@ from app.database import add_product_to_order, create_order_with_items, get_orde
 router = APIRouter(prefix="/orders", tags=["Orders"])
 
 
+@router.get("/", response_model=list[OrderOut], status_code=status.HTTP_200_OK)
+async def get_all_orders(user: TokenData = Depends(get_current_user)):
+    orders = get_orders_by_user(user.id_usuario)
+    return orders
+
+
+"""
 @router.post("/", response_model=OrderOut, status_code=status.HTTP_201_CREATED)
 async def create_new_order(
     order: OrderCreate,
@@ -28,14 +35,9 @@ async def create_new_order(
         raise HTTPException(
             status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
         )
+"""
 
-
-@router.get("/", response_model=list[OrderOut], status_code=status.HTTP_200_OK)
-async def get_all_orders(user: TokenData = Depends(get_current_user)):
-    orders = get_orders_by_user(user.id_usuario)
-    return orders
-
-
+"""
 @router.get("/{order_id}", response_model=OrderOut, status_code=status.HTTP_200_OK)
 async def get_order_by_user(
     order_id: int,
@@ -47,4 +49,4 @@ async def get_order_by_user(
         raise HTTPException(
             status_code=status.HTTP_404_NOT_FOUND,
         )
- 
+"""
