cfb6c4362c410ccff2fd1f6a11697c34049c4d8f
Egnyeh <egnyeh@gmail.com>
2026-01-19 18:21:28 +0100

    Revert "Fixes."
    This reverts commit 8ae2b536805f0aec75fbfccf42e08d0fb27c178f.

--- template/app/routers/orders.py
+++ template/app/routers/orders.py
@@ -11,6 +11,7 @@ from app.models import OrderCreate, OrderOut
 
 router = APIRouter(prefix="/orders", tags=["Orders"])
 
+
 @router.post("/", response_model=OrderOut, status_code=status.HTTP_201_CREATED)
 async def create_order(
     order: OrderCreate,
@@ -79,7 +80,24 @@ async def create_order(
     if numero_pedido is None:
         raise HTTPException(
             status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
-            detail="Order could not be created"
         )
 
-    return OrderOut(numero_pedido=numero_pedido)
+
+@router.get("/", response_model=list[OrderOut], status_code=status.HTTP_200_OK)
+async def get_all_orders(user: TokenData = Depends(get_current_user)):
+    orders = get_orders_by_user(user.id_usuario)
+    return orders
+
+
+@router.get("/{order_id}", response_model=OrderOut, status_code=status.HTTP_200_OK)
+async def get_order_by_user(
+    order_id: int,
+    user: TokenData = Depends(get_current_user)
+):
+    orders = get_orders_by_user(user.id_usuario)
+    order = next((o for o in orders), None)
+    if not order:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+        )
+ 
